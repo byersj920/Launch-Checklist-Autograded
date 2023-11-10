c9ba3831772c03bca7048ab2df872b2f603c7713
Jonathan Byers <byersj920@gmail.com>
2023-11-09 20:17:34 -0600

    formSubmission function now displays proper alerts for errors

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Write your JavaScript code here!
+/* const { formSubmission } = require("./scriptHelper"); */
 
 window.addEventListener("load", function() {
 
@@ -13,4 +13,17 @@ window.addEventListener("load", function() {
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
     })
     
+
+    /* Adding the formSubmission() function here.*/
+    /* The form submission function does work properly. However, it will not work if the previous lines of texts have errors.
+    Once the rest of the graded assignment is worked on, this should properly do alerts. */
+    let form = document.getElementById("testForm");
+    form.addEventListener('submit', function(){
+        let pilot = document.querySelector("input[name=pilotName]");
+        let copilot = document.querySelector("input[name=copilotName]");
+        let fuelLevel = document.querySelector("input[name=fuelLevel]");
+        let cargoLevel = document.querySelector("input[name=cargoMass]");
+        formSubmission("document", "list for later", pilot.value, copilot.value, fuelLevel.value, cargoLevel.value);
+    });
+
  });
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,13 +17,43 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
     */
  }
  
+ /* So, for the formSubmission project: My plan is to use the function validateInput to return a validation string for what is
+ being tested. This checks what type of submission each entry is. Then, I can use this function in formSubmission() to verify
+ that each entry is yielding the correct validation string. Both of these will execute within the scripts.js file after
+ the form.addEventListener('submit', function(){}) event.*/
  function validateInput(testInput) {
     
+    if (testInput.length === 0){
+        return "Empty";
+    } if (isNaN(testInput) === true){
+        return "Not a Number";
+    } else {
+        return "Is a Number";
+    }
  }
  
  function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    
- }
+    if(validateInput(pilot) === "Empty"){
+        window.alert("Pilot name cannot be empty.");
+    } else if (validateInput(pilot) === "Is a Number"){
+        window.alert(`${pilot} is not a valid name. That's a number.`);
+    }
+    if(validateInput(copilot) === "Empty"){
+        window.alert("Copilot name cannot be empty.");
+    } else if (validateInput(copilot) === "Is a Number"){
+        window.alert(`${copilot} is not a valid name. That's a number.`);
+    }
+    if(validateInput(fuelLevel) === "Empty"){
+        window.alert("Please add a fuel level.");
+    } else if (validateInput(fuelLevel) === "Not a Number"){
+        window.alert("Please submit a valid number for your fuel level.");
+    }
+    if(validateInput(cargoLevel) === "Empty"){
+        window.alert("Please add a cargo level.");
+    } else if (validateInput(cargoLevel) === "Not a Number"){
+        window.alert("Please submit a valid number for your cargo level.");
+    }
+}
  
  async function myFetch() {
      let planetsReturned;
