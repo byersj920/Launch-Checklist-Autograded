80ad3d71faecf4292a922ed0ecf2f10427bf174b
Jonathan <byersj920@gmail.com>
2023-11-10 21:40:51 -0600

    Tests 1-6 are now passing

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 /* const { formSubmission } = require("./scriptHelper"); */
 
+
+
+
 window.addEventListener("load", function() {
+    let form = document.getElementById("testForm");
+    let pilot = document.querySelector("input[name=pilotName]");
+    let copilot = document.querySelector("input[name=copilotName]");
+    let fuelLevel = document.querySelector("input[name=fuelLevel]");
+    let cargoLevel = document.querySelector("input[name=cargoMass]");
+
+
+    form.addEventListener('submit', function(){
+        formSubmission(document, "list for later", pilot.value, copilot.value, fuelLevel.value, cargoLevel.value)
+        event.preventDefault();
+    });
+    
+
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
@@ -17,13 +33,6 @@ window.addEventListener("load", function() {
     /* Adding the formSubmission() function here.*/
     /* The form submission function does work properly. However, it will not work if the previous lines of texts have errors.
     Once the rest of the graded assignment is worked on, this should properly do alerts. */
-    let form = document.getElementById("testForm");
-    form.addEventListener('submit', function(){
-        let pilot = document.querySelector("input[name=pilotName]");
-        let copilot = document.querySelector("input[name=copilotName]");
-        let fuelLevel = document.querySelector("input[name=fuelLevel]");
-        let cargoLevel = document.querySelector("input[name=cargoMass]");
-        formSubmission("document", "list for later", pilot.value, copilot.value, fuelLevel.value, cargoLevel.value);
-    });
+    
 
  });
--- scriptHelper.js
+++ scriptHelper.js
@@ -33,25 +33,75 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
  }
  
  function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    
     if(validateInput(pilot) === "Empty"){
         window.alert("Pilot name cannot be empty.");
+        return;
     } else if (validateInput(pilot) === "Is a Number"){
         window.alert(`${pilot} is not a valid name. That's a number.`);
+        return;
     }
     if(validateInput(copilot) === "Empty"){
         window.alert("Copilot name cannot be empty.");
+        return;
     } else if (validateInput(copilot) === "Is a Number"){
         window.alert(`${copilot} is not a valid name. That's a number.`);
+        return;
     }
     if(validateInput(fuelLevel) === "Empty"){
         window.alert("Please add a fuel level.");
+        return;
     } else if (validateInput(fuelLevel) === "Not a Number"){
         window.alert("Please submit a valid number for your fuel level.");
+        return;
     }
     if(validateInput(cargoLevel) === "Empty"){
         window.alert("Please add a cargo level.");
+        return;
     } else if (validateInput(cargoLevel) === "Not a Number"){
         window.alert("Please submit a valid number for your cargo level.");
+        return;
+    } else {
+
+        /*This else statement will execute only if there are no errors with the user submissions. This will make 
+        the launchStatus list visible, as well as use what the user submitted to determine success or failure 
+        on a mission.*/
+        let launchStatus = document.getElementById("launchStatus");
+        let faultyItems = document.getElementById("faultyItems");
+        let pilotStatus = document.getElementById("pilotStatus");
+        let copilotStatus = document.getElementById("copilotStatus");
+        let fuelStatus = document.getElementById("fuelStatus");
+        let cargoStatus = document.getElementById("cargoStatus");
+        faultyItems.style.visibility = "visible";
+        pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+        copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+
+
+        /* Note for Demo: These unit tests were AWFUL. Not only did they require specific text to be put in
+        each statement (which was not instructed to us btw) but because the tests each change the html doc,
+        each change to the text carried OVER to the next test. Which, isn't hard to fix. But with that information
+        being left out of the instructions made getting these tests to pass very frustrating. */
+        if (fuelLevel < 10000){
+            launchStatus.style.color = 'red';
+            launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+            fuelStatus.innerHTML = 'Fuel level too low for launch';
+        } else {
+            fuelStatus.innerHTML = 'Fuel level high enough for launch';
+        }
+
+        if (cargoLevel > 10000){
+            launchStatus.style.color = 'red';
+            launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+            cargoStatus.innerHTML = 'Cargo mass too heavy for launch';
+        } else {
+            cargoStatus.innerHTML = "Cargo mass low enough for launch";
+        }
+
+        if ((fuelLevel >= 10000) && (cargoLevel <= 10000)){
+            launchStatus.style.color = 'green';
+            launchStatus.innerHTML = "Shuttle is Ready for Launch";
+        }
+
     }
 }
  
